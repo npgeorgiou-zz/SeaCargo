75a98b3d5d4c8e7e1139a936badb8831181b08ee
Nikos <npgeorgiou@yahoo.gr>
2014-12-22 02:02:47 +0100

    bug fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var favicon = require('serve-favicon');
 var routes = require('./routes/index');
 var orders = require('./routes/orders');
 var orderdetails = require('./routes/orderdetails');
@@ -18,8 +19,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
+//app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
